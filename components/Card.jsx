306df11eed3708d345db394db9c9729416218495
unknown <robbyihza@gmail.com>
2023-10-09 02:33:52 +0700

    done

--- components/Card.jsx
+++ components/Card.jsx
@@ -19,7 +19,7 @@ export default function Card() {
         }
     ];
     return (
-        <div className="container mx-auto mt-32">
+        <div className="container mx-auto mt-32 mb-28">
             <h1 className="text-3xl font-semibold mb-4">Kartu Sederhana</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {cards.map((card, index) => (
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className=" p-24 text-center text-white bg-slate-800">
-            Made in Japan
+        <div className=" p-4 text-center text-white bg-slate-800">
+            Thanks TalentHub 🫵
         </div>
     )
 }
